3834b98956f3a06f5cff14bb3a0fdb166e8f3d55
mitka0260 <mitkaut@gmail.com>
2020-12-13 19:00:45 +0200

    commit

---
+++ src/21Functions/Harmonic2111.java
@@ -0,0 +1,20 @@
+public class Harmonic2111 {
+
+//java Harmonic211 1 2 4
+
+	public static double harmonic (int n) {
+		double sum = 0.0;
+		for (int i = 1; i<=n; i++)
+			sum += 1.0/i;
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i<args.length; i++) {
+			int arg = Integer.parseInt(args[i]);
+			double value = harmonic(arg);
+			StdOut.println(value);
+		}
+
+	}
+}
